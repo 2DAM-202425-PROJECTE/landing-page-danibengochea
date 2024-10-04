e161b30b37127d4e8c11008f52cf20dece7b3d00
danibengocheaa <danibengochea@iesebre.com>
2024-10-04 15:18:22 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 export default {
-// config options
-<<<<<<< HEAD
-base: '/landing-page-dani/'
-=======
-base: '/landing-page-danibengochea/'
->>>>>>> c24e884578393249b6d9c5d5a1c9df288d368cca
+  // Config options
+  base: '/landing-page-danibengochea/'
 }
+
